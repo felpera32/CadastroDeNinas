de4620722b7c7554f78bd6b7c55307e2c909e001
felpera32 <contatojpvs@gmail.com>
2026-03-05 21:22:58 -0300

    feat: Adiciona MissoesRepository

---
+++ src/main/java/dev/felpus/CadastroDePiratas/Missoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package dev.felpus.CadastroDePiratas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository  extends JpaRepository<MissoesModel, Long> {
+}
