ded914bdc4071b9c0f539f0baae73c60734ad3a9
felpera32 <contatojpvs@gmail.com>
2026-03-12 15:46:41 -0300

    feat: Cria as rotas do controller com o mock

---
+++ src/main/java/dev/felpus/CadastroDePiratas/Missoes/MissoesController.java
@@ -0,0 +1,34 @@
+package dev.felpus.CadastroDePiratas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    //GET -- Mandar uma requisição para mostrar as missões
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Missão listadas com sucesso";
+    }
+
+
+    //POST -- Mandar uma requisição para criar as missões
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão Criada com sucesso";
+    }
+
+
+    //PUT -- Mandar uma requisição para alterar as missões
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missão alterada com sucesso";
+    }
+
+    //DELETE -- Mandar uma requisição para deletar as missões
+    @DeleteMapping
+    public String deletarMissao(){
+        return "Missão deletada com sucesso";
+    }
+}
