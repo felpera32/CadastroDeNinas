d736243887c1de5dc4b71e6a74d51c9362fc238a
felpera32 <contatojpvs@gmail.com>
2026-03-19 18:12:57 -0300

    feat: Adiciona como criar, listar, listar por id e deletar missoes no MissoesServices

--- src/main/java/dev/felpus/CadastroDePiratas/Missoes/MissoesController.java
+++ src/main/java/dev/felpus/CadastroDePiratas/Missoes/MissoesController.java
@@ -1,24 +1,35 @@
 package dev.felpus.CadastroDePiratas.Missoes;
-
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("missoes")
 public class MissoesController {
 
+    private MissoesService missaoService;
+
+    public MissoesController(MissoesService missaoService) {
+        this.missaoService = missaoService;
+    }
+
     //GET -- Mandar uma requisição para mostrar as missões
     @GetMapping("/listar")
-    public String listarMissao(){
-        return "Missão listadas com sucesso";
+    public List<MissoesModel> listarMissao(){
+        return missaoService.listarMissoes();
     }
 
 
     //POST -- Mandar uma requisição para criar as missões
     @PostMapping("/criar")
-    public String criarMissao(){
-        return "Missão Criada com sucesso";
+    public MissoesModel criarMissao(@RequestBody MissoesModel missao){
+        return missaoService.criarMissao(missao);
     }
 
+    //Listar por id
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarMissaoPorId(@PathVariable Long id){
+        return missaoService.listarPorId(id);
+    }
 
     //PUT -- Mandar uma requisição para alterar as missões
     @PutMapping("/alterar")
@@ -27,7 +38,8 @@ public class MissoesController {
     }
 
     //DELETE -- Mandar uma requisição para deletar as missões
-    public String deletarMissao(){
-        return "Missão deletada com sucesso";
+    @DeleteMapping("/deletar/{id}")
+    public void deletarMissao(@PathVariable Long id){
+        missaoService.deletarMissao(id);
     }
 }
---
+++ src/main/java/dev/felpus/CadastroDePiratas/Missoes/MissoesService.java
@@ -0,0 +1,37 @@
+package dev.felpus.CadastroDePiratas.Missoes;
+
+import dev.felpus.CadastroDePiratas.Ninjas.NinjaModel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/missoes")
+public class MissoesService {
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    //Criar
+    public MissoesModel criarMissao(MissoesModel missao){
+        return missoesRepository.save(missao);
+    }
+    //Listar
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+    //listar por id
+    public MissoesModel listarPorId(Long id){
+        Optional<MissoesModel> missaoPorId = missoesRepository.findById(id);
+        return missaoPorId.orElse(null);
+    }
+    //Deletar
+    public void deletarMissao(Long id){
+        missoesRepository.deleteById(id);
+    }
+    //alterar
+}
